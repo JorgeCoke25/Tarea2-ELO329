859e8b823473ad902f74da552b2bc2e37fc15c48
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-05-27 19:40:15 -0400

    Initial Commit

---
+++ src/main/java/com/example/tarea2/LampControlView.java
@@ -0,0 +1,14 @@
+package com.example.tarea2;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class LampControlView extends VBox {
+    public LampControlView(LampControl lampControl) {
+        Image img = new Image("rsc/powerImage.jpg");
+        ImageView view = new ImageView(img);
+        // ¿?
+    }
+}
